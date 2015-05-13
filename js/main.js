b780e8479fcc61b294f288a0113d0dd65c845d67
diandian <diandian@futunn.com>
2015-05-13 14:13:18 +0800

    拖拽功能添加

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ function My_AlertBox(str,mydialog_fn){
 	this.myDialog_newDiv.id=mydialog_Btn;
 
 	this.myDialog_newDiv.innerHTML = '<div class="mydialog_mask" id="alerMask"></div>\
-	<div class="mydialog_dialog mydialog_borderRadius">\
+	<div class="mydialog_dialog mydialog_borderRadius" onselectstart="return false">\
 	<h3>温馨提示:</h3>\
 	<div class="mydialog_Content">\
 		<p>\
@@ -66,6 +66,7 @@ function My_AlertBox(str,mydialog_fn){
 
 }
 
+//弹框出现、消失
 My_AlertBox.prototype.alertConfirm = function(){
 	this.myDialog_newDiv.style.display = 'block';		
 };
@@ -97,32 +98,69 @@ My_AlertBox.prototype.myDialogDragUp = function(){
 	document.onmouseup = null;
 }*/
 
+//拖拽
+
 var eventHandler = {};
 
 My_AlertBox.prototype.myDialogDragDown = function(oEvent){
-	this.disX = oEvent.clientX-this.DragObj.offsetLeft;
-	this.disY = oEvent.clientY-this.DragObj.offsetTop;
 	var _this = this;
+
 	eventHandler.mousemove = function(e){
 		_this.mydialog_mousemove(e, _this);
 	};
 	eventHandler.mouseup = function(e){
-		console.log('mouseup');
+		// console.log('mouseup');
 		_this.mydialog_mouseup(e, _this);
 	};
+	//获取绝对位置
+	eventHandler.getPosition = function(obj){
+		var DragObjLeft = 0;
+		var DragObjTop = 0;
+		while(obj){
+			DragObjLeft += obj.offsetLeft;
+			DragObjTop += obj.offsetTop;
+			obj = obj.offsetParent;
+		}
+		return {l:DragObjLeft,t:DragObjTop};
+	};
+
+	this.disX = oEvent.clientX-eventHandler.getPosition(this.DragObj).l;
+	this.disY = oEvent.clientY-eventHandler.getPosition(this.DragObj).t;
+
 	document.addEventListener('mousemove',eventHandler.mousemove,false);
-	document.addEventListener('mouseup',eventHandler.mouseup,false);	
+	document.addEventListener('mouseup',eventHandler.mouseup,false);
+
+	if(this.DragObj.setCapture)this.DragObj.setCapture();
+	return false;
 };
 
 My_AlertBox.prototype.myDialogDragMove = function(oEvent){
-	this.DragObj.style.left = oEvent.clientX-this.disX+230+'px';
-	this.DragObj.style.top = oEvent.clientY-this.disY+100+'px';
+	this.left = oEvent.clientX-this.disX;
+	this.top = oEvent.clientY-this.disY;
+
+	this.clientWidth = document.documentElement.clientWidth||document.body.clientWidth;
+	this.clientHeight = document.documentElement.clientHeight||document.body.clientHeight;
+	if(this.left<50){
+		this.left=0
+	}
+	else if(this.left>this.clientWidth-this.DragObj.offsetWidth-50){
+		this.left = this.clientWidth-this.DragObj.offsetWidth
+	}
+	if(this.top<50){
+		this.top=0;
+	}
+	else if(this.top>this.clientHeight-this.DragObj.offsetHeight-50){
+		this.top = this.clientHeight-this.DragObj.offsetHeight
+	}
+	this.DragObj.style.left = this.left+'px';
+	this.DragObj.style.top = this.top+'px';
 };
 
 My_AlertBox.prototype.myDialogDragUp = function(){
-	console.log('ready to unbind');
+	// console.log('ready to unbind');
 	document.removeEventListener('mousemove',eventHandler.mousemove,false);
 	document.removeEventListener('mouseup',eventHandler.mouseup,false);
+	if(this.DragObj.releaseCapture)this.DragObj.releaseCapture();
 };
 
 My_AlertBox.prototype.mydialog_mousemove = function(ev,_this){
@@ -131,7 +169,7 @@ My_AlertBox.prototype.mydialog_mousemove = function(ev,_this){
 };
 
 My_AlertBox.prototype.mydialog_mouseup = function(ev,_this){
-	console.log('mydialog_mouseup');
+	// console.log('mydialog_mouseup');
 	_this.myDialogDragUp();
 };
 
